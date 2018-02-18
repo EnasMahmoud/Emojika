5cc7a82e0f4800ab144cd3b020bff1d18ceb8959
EnasMahmoud <35309733+EnasMahmoud@users.noreply.github.com>
2018-02-18 20:38:31 +0200

    Initial commit

---
+++ app/src/main/java/pro/phoenix/emojika/MeetingMenuActivity.java
@@ -0,0 +1,13 @@
+package pro.phoenix.emojika;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeetingMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting_menu);
+    }
+}
